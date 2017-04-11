70e5fe7b32a46fe2975e6a13db5dc9d3fa1c450a
Maksym Koval <pretorian1@i.ua>
2017-04-12 00:05:06 +0300

    First commit

---
+++ app/src/main/java/com/example/max/recordplayvisualizationsound/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.max.recordplayvisualizationsound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.crash.FirebaseCrash;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseCrash.report(new Exception("My first Android non-fatal error"));//crash reporting
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-5405208829283027~3111027393");
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
